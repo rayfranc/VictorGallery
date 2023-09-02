a92f1cd8eaa1574e82de8634c0139471669e42f2
Ray Francisco <rayfranciscoplay@gmail.com>
2023-09-02 16:30:57 -0400

    Page Transition

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import '@fontsource/anton'
 import '@fontsource/adamina'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import Events from './pages/Events'
 
--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-  rollupOptions: {
-    external: ["react","react-router", "react-router-dom"],
-    output: {
-      globals: {
-        react: "React",
-      }
-    }
-  }
-}
+  plugins: [react()]
 })
